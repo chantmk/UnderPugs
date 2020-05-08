5d808399c15e79c80f0e95b502d9619912e9dad8
GottaK <chan_tham@hotmail.com>
2020-05-08 11:14:42 +0700

    initial commit

---
+++ Final_project.srcs/sources_1/new/TopSystem.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/08/2020 10:57:14 AM
+// Design Name: 
+// Module Name: TopSystem
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module TopSystem(
+    input clk,
+    input RsRx,
+    output RsTx,
+    output Hsync,
+    output Vsync,
+    output vgaRed,
+    output vgaGreen,
+    output vgaBlue
+    );
+endmodule
