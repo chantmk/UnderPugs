39a246c6836e0db5e287c5a047eda36c951950dc
Owenthanapat <owenthanapat229@gmail.com>
2020-06-06 22:55:27 +0700

    fix are play

--- Final_project.srcs/sources_1/new/defState.v
+++ Final_project.srcs/sources_1/new/defState.v
@@ -49,6 +49,7 @@ module defState(
     reg [2:0] checkbullet;
     reg VChangeState;
     reg [7:0]counter;
+    reg [1:0]previousMonsType;
     assign bulletPosX = {abx,bbx,cbx};
     assign bulletPosY = {aby,bby,cby};
     
@@ -88,6 +89,8 @@ module defState(
     
     // MC 16x16 bullet 16x16 and 32x32 and 32x140 and dynamic
     always @(posedge game_clk)
+        begin
+        if(previousMonsType == monsterType)
         begin
         casez({isCollision,monsterType})// assume play area 256x208 from (192,210)to(448,418) and MC 16x16
             5'b001??:
@@ -239,72 +242,72 @@ module defState(
                                 aby = aby-1;
                                 bby = bby-1;
                                 cby = cby-1;
-                                if(aby==210)up_down[2]=1;
-                                if(bby==210)up_down[1]=1;
-                                if(cby==210)up_down[0]=1;
+                                if(aby==196)up_down[2]=1;
+                                if(bby==196)up_down[1]=1;
+                                if(cby==196)up_down[0]=1;
                             end
                         3'b001:
                             begin
                                 aby = aby-1;
                                 bby = bby-1;
                                 cby = cby+1;
-                                if(aby==210)up_down[2]=1;
-                                if(bby==210)up_down[1]=1;
-                                if(cby==386)up_down[0]=0;
+                                if(aby==196)up_down[2]=1;
+                                if(bby==196)up_down[1]=1;
+                                if(cby==371)up_down[0]=0;
                             end
                         3'b010:
                             begin
                                 aby = aby-1;
                                 bby = bby+1;
                                 cby = cby-1;
-                                if(aby==210)up_down[2]=1;
-                                if(bby==386)up_down[1]=0;
-                                if(cby==210)up_down[0]=1;
+                                if(aby==196)up_down[2]=1;
+                                if(bby==371)up_down[1]=0;
+                                if(cby==196)up_down[0]=1;
                             end
                         3'b011:
                             begin
                                 aby = aby-1;
                                 bby = bby+1;
                                 cby = cby+1;
-                                if(aby==210)up_down[2]=1;
-                                if(bby==386)up_down[1]=0;
-                                if(cby==386)up_down[0]=0;
+                                if(aby==196)up_down[2]=1;
+                                if(bby==371)up_down[1]=0;
+                                if(cby==371)up_down[0]=0;
                             end
                         3'b100:
                             begin
                                 aby = aby+1;
                                 bby = bby-1;
                                 cby = cby-1;
-                                if(aby==386)up_down[2]=0;
-                                if(bby==210)up_down[1]=1;
-                                if(cby==210)up_down[0]=1;
+                                if(aby==371)up_down[2]=0;
+                                if(bby==196)up_down[1]=1;
+                                if(cby==196)up_down[0]=1;
                             end
                         3'b101:
                             begin
                                 aby = aby+1;
                                 bby = bby-1;
                                 cby = cby+1;
-                                if(aby==386)up_down[2]=0;
-                                if(bby==210)up_down[1]=1;
-                                if(cby==386)up_down[0]=0;
+                                if(aby==371)up_down[2]=0;
+                                if(bby==196)up_down[1]=1;
+                                if(cby==371)up_down[0]=0;
                             end
                         3'b110:
                             begin
                                 aby = aby+1;
                                 bby = bby+1;
                                 cby = cby-1;
-                                if(aby==386)up_down[2]=0;
-                                if(bby==386)up_down[1]=0;
-                                if(cby==210)up_down[0]=1;
+                                if(aby==371)up_down[2]=0;
+                                if(bby==371)up_down[1]=0;
+                                if(cby==196)up_down[0]=1;
                             end
                         3'b111:
                             begin
                                 aby = aby+1;
                                 bby = bby+1;
                                 cby = cby+1;
-                                if(aby==386)up_down[2]=0;
-                                if(bby==386)up_down[1]=0;
-                                if(cby==386)up_down[0]=0;
+                                if(aby==371)up_down[2]=0;
+                                if(bby==371)up_down[1]=0;
+                                if(cby==371)up_down[0]=0;
                             end  
                     endcase
                 end
@@ -313,20 +316,11 @@ module defState(
                     aby = aby-1;
                     bby = bby-1;
                     cby = cby-1;
-                    if(aby==210)aby= 278;
-                    if(bby==210)bby= 278;
-                    if(cby==210)cby= 278;
+                    if(aby==196)aby= 278;
+                    if(bby==196)bby= 278;
+                    if(cby==196)cby= 278;
                 end
-            2'b11://16x140
-                begin
-                    aby = aby-1;
-                    bby = bby-1;
-                    cby = cby-1;
-                    if(aby==210)aby= 278;
-                    if(bby==210)bby= 278;
-                    if(cby==210)cby= 278;
-                end
-            2'b11:
+            5'b00011:
                 begin
                     case(left_right)
                         3'b000:
@@ -405,35 +399,90 @@ module defState(
                 end
         endcase
         end
+        else
+        begin
+            case(monsterType)
+            2'b00:
+            begin
+                abx = 222;
+                aby = 222;
+                bbx = 348;
+                bby = 333;
+                cbx = 278;
+                cby = 388;
+                left_right = 3'b101;
+            end
+            2'b01:
+            begin
+                abx = 300;
+                aby = 250;
+                bbx = 400;
+                bby = 380;
+                cbx = 278;
+                cby = 388;
+                up_down = 3'b001;
+            end
+            2'b10:
+            begin
+                abx = 193;
+                aby = 250;
+                bbx = 193;
+                bby = 380;
+                cbx = 263;
+                cby = 388;
+            end
+            2'b01:
+            begin
+                abx = 300;
+                aby = 250;
+                bbx = 400;
+                bby = 380;
+                cbx = 278;
+                cby = 388;
+            end
+            2'b01:
+            begin
+                abx = 300;
+                aby = 250;
+                bbx = 400;
+                bby = 380;
+                cbx = 278;
+                cby = 388;
+                left_right = 3'b110;
+            end
+            endcase
+            previousMonsType = monsterType;
+        end
+        end
     always @(posedge game_clk)
         begin
-        case(direction)// assume play area 256x208 from (192,210)to(448,418) and MC 16x16
+        case(direction)// assume play area 256x208 from (192,196)to(448,403) and MC 16x16
             5'b00001: 
                 begin
                     if(xCurrent > 192)xCurrent <= xCurrent-1;//A
                     if(monsterType == 2'b11)
                         begin
-                            if(yCurrent<=401)yCurrent =yCurrent+1;
+                            if(yCurrent<=387)yCurrent =yCurrent+1;
                         end
                 end
             5'b00010: 
                 begin
-                    if(yCurrent > 210)yCurrent <= yCurrent-1;//W
+                    if(yCurrent > 196)yCurrent <= yCurrent-1;//W
                 end
             5'b00100: 
                 begin
                     if(monsterType == 2'b11)
                         begin
-                            if(yCurrent<=401)yCurrent =yCurrent+2;
+                            if(yCurrent<=386)yCurrent =yCurrent+2;
                         end
-                    else if(yCurrent < 402)yCurrent <= yCurrent+1;//D
+                    else if(yCurrent < 387)yCurrent <= yCurrent+1;//D
                 end
             5'b01000: 
                 begin
                     if(xCurrent < 432)xCurrent <= xCurrent+1;//D
                     if(monsterType == 2'b11)
                         begin
-                            if(yCurrent<=401)yCurrent =yCurrent+1;
+                            if(yCurrent<=386)yCurrent =yCurrent+1;
                         end
                 end
 //            5'b10000: 
